7ee6315358124c91f2f39d07b1475779242e3bbf
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-17 02:35:18 +0500

    modal css

--- partials/register.php
+++ partials/register.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content registerModal">
             <form>
-                <div class="modal-header">
+                <div class="modal-header border-0">
                     <h5 class="modal-title d-flex align-items-center">
                         <i class="bi bi-person-lines-fill fs-3 me-2"></i>
                         User Registration
@@ -10,18 +10,15 @@
                     <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-                        Please ensure that your email address is accurate and valid. Only enter correct and valid information
-                    </span>
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6">
                                 <label class="form-label">Name</label>
-                                <input type="text" class="form-control shadow-none">
+                                <input type="text" class="bg-transparent border-dark form-control shadow-none">
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Email address</label>
-                                <input type="email" class="form-control shadow-none">
+                                <input type="email" class="bg-transparent border-dark form-control shadow-none">
                             </div>
                         </div>
                     </div>
