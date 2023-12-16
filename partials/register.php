5a1cf13eda1712fc6b05b384fd9b31c66ec56275
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-17 02:04:28 +0500

    image for register modal

--- partials/register.php
+++ partials/register.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content registerModal">
             <form>
                 <div class="modal-header">
                     <h5 class="modal-title d-flex align-items-center">
@@ -10,17 +10,20 @@
                     <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control shadow-none">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between mb-2">
-                        <button type="submit" class="py-1 px-3 border-0 rounded-1 btn-orange text-white">Login</button>
-                        <a href="javascript: void(0)" class="text-primary text-decoration-none">Forgot Password?</a>
+                    <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                        Please ensure that your email address is accurate and valid. Only enter correct and valid information
+                    </span>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label">Name</label>
+                                <input type="text" class="form-control shadow-none">
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Email address</label>
+                                <input type="email" class="form-control shadow-none">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </form>
