dced0bb6b785cc20f78fcdcd889f3ec7cd7b49af
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-19 02:06:51 +0500

    form responsive

--- partials/availability.php
+++ partials/availability.php
@@ -1,25 +1,35 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 bg-white shadow p-4 rounded">
-            <h5>Check Booking Availability</h5>
+            <h5 class="mb-4">Check Booking Availability</h5>
             <form>
-                <div class="row">
-                    <div class="col-lg-3">
+                <div class="row align-items-end">
+                    <div class="col-lg-3 mb-3">
                         <label class="form-label" style="font-weight: 500;">Check In</label>
                         <input type="date" class="form-control shadow-none">
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-lg-3 mb-3">
                         <label class="form-label" style="font-weight: 500;">Check Out</label>
                         <input type="date" class="form-control shadow-none">
                     </div>
-                    <div class="col-lg-3">
-                        <label class="form-label" style="font-weight: 500;">Adult</label>
-                        <select class="form-select">
-                            <option selected>Open this select menu</option>
+                    <div class="col-lg-2 mb-3">
+                        <label class="form-label" style="font-weight: 500;">Adults</label>
+                        <select class="form-select shadow-none">
                             <option value="1">One</option>
                             <option value="2">Two</option>
                             <option value="3">Three</option>
                         </select>
+                    </div>
+                    <div class="col-lg-2 mb-3">
+                        <label class="form-label" style="font-weight: 500;">Childrens</label>
+                        <select class="form-select shadow-none">
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="col-lg-1">
+                        <button type="submit" class="py-1 px-3 border-0 rounded-1 btn-orange text-white">Submit</button>
                     </div>
                 </div>
             </form>
