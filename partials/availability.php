6cd2f5ce25c57e354023b0012bb7de914a35a910
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-19 01:51:59 +0500

    form additions

---
+++ partials/availability.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 bg-white shadow p-4 rounded">
+            <h5>Check Booking Availability</h5>
+            <form>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <label class="form-label" style="font-weight: 500;">Check In</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="col-lg-3">
+                        <label class="form-label" style="font-weight: 500;">Check Out</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="col-lg-3">
+                        <label class="form-label" style="font-weight: 500;">Adult</label>
+                        <select class="form-select">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
