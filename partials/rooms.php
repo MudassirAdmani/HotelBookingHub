09622456a45a94272c37c6bc535fd2c5aa507185
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-21 01:01:11 +0500

    Rooms Completed

--- partials/rooms.php
+++ partials/rooms.php
@@ -54,7 +54,113 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-12 text-center mt-5">
+        <div class="col-lg-4 col-md-6 my-3">
+            <div class="card border-0 shadow" style="max-width: 320px; margin: auto;">
+                <img src="./images/room-2.png" style="height: 300px; object-fit: cover;" class="card-img-top" alt="room-1-image">
+                <div class="card-body">
+                    <h5>Simple Room</h5>
+                    <h6 class="mb-4">200$ per night</h6>
+                    <div class="features mb-4">
+                        <h6 class="mb-1">Features</h6>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            2 Rooms
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            1 Bathroom
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            1 Balcony
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Breakfast
+                        </span>
+                    </div>
+                    <div class="facilities mb-4">
+                        <h6 class="mb-1">Facilities</h6>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Wifi
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Smart TV
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            AC
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Heater
+                        </span>
+                    </div>
+                    <div class="rating mb-4">
+                        <h6>Rating</h6>
+                        <span class="badge rounded-pill bg-light">
+                            <i class="bi me-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi ms-1 bi-star-fill text-warning fs-4"></i>
+                        </span>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-evenly mb-2">
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Now</a>
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-purple text-white">See Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 my-3">
+            <div class="card border-0 shadow" style="max-width: 320px; margin: auto;">
+                <img src="./images/room-3.png" style="height: 300px; object-fit: cover;" class="card-img-top" alt="room-1-image">
+                <div class="card-body">
+                    <h5>Simple Room</h5>
+                    <h6 class="mb-4">200$ per night</h6>
+                    <div class="features mb-4">
+                        <h6 class="mb-1">Features</h6>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            2 Rooms
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            1 Bathroom
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            1 Balcony
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Breakfast
+                        </span>
+                    </div>
+                    <div class="facilities mb-4">
+                        <h6 class="mb-1">Facilities</h6>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Wifi
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Smart TV
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            AC
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Heater
+                        </span>
+                    </div>
+                    <div class="rating mb-4">
+                        <h6>Rating</h6>
+                        <span class="badge rounded-pill bg-light">
+                            <i class="bi me-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi ms-1 bi-star-fill text-warning fs-4"></i>
+                        </span>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-evenly mb-2">
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Now</a>
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-purple text-white">See Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12 text-center mt-4">
             <a href="" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">See More >>></a>
         </div>
     </div>
