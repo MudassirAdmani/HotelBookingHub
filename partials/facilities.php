080a553703518adcf0445cdfe4e9a54eef220a42
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-21 01:55:15 +0500

    facilities completed

--- partials/facilities.php
+++ partials/facilities.php
@@ -1,4 +1,4 @@
-<h2 class="my-4 pt-4 text-center fw-bold h-font">Our Rooms</h2>
+<h2 class="my-4 pt-4 text-center fw-bold h-font">Our Facilities</h2>
 <div class="container">
     <div class="row justify-content-evenly px-md-0 px-5">
         <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
@@ -10,8 +10,8 @@
             <h5>Buffet</h5>
         </div>
         <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
-            <img src="./images/wifi-1.png" alt="">
-            <h5>Wifi</h5>
+            <img src="./images/kit-1.png" alt="">
+            <h5>Hygiene Kit</h5>
         </div>
         <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
             <img src="./images/wifi-1.png" alt="">
