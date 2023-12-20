ae98995e11fee45d1fe2668c41f692d074b7b776
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-20 20:43:55 +0500

    room card completed

--- partials/rooms.php
+++ partials/rooms.php
@@ -2,8 +2,8 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-4 col-md-6 my-3">
-            <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
-                <img src="./images/room-1.png" class="card-img-top" alt="room-1-image">
+            <div class="card border-0 shadow" style="max-width: 320px; margin: auto;">
+                <img src="./images/room-1.png" style="height: 300px; object-fit: cover;" class="card-img-top" alt="room-1-image">
                 <div class="card-body">
                     <h5>Simple Room</h5>
                     <h6 class="mb-4">200$ per night</h6>
@@ -22,7 +22,35 @@
                             Breakfast
                         </span>
                     </div>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="facilities mb-4">
+                        <h6 class="mb-1">Facilities</h6>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Wifi
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Smart TV
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            AC
+                        </span>
+                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                            Heater
+                        </span>
+                    </div>
+                    <div class="rating mb-4">
+                        <h6>Rating</h6>
+                        <span class="badge rounded-pill bg-light">
+                            <i class="bi me-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi mx-1 bi-star-fill text-warning fs-4"></i>
+                            <i class="bi ms-1 bi-star-fill text-warning fs-4"></i>
+                        </span>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-evenly mb-2">
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Now</a>
+                        <a href="#" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-purple text-white">See Details</a>
+                    </div>
                 </div>
             </div>
         </div>
