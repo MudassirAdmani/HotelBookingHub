c12d6be1c688693e173c493b4bc16f3eb5580c5c
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-22 00:58:28 +0500

    swiper script added

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ var swiper = new Swiper(".mainSwiper", {
 });
 var swiper2 = new Swiper(".testimonialSwiper", {
   effect: "coverflow",
+  spaceBetween: 100,
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: "3",
+  loop: true,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -19,4 +20,18 @@ var swiper2 = new Swiper(".testimonialSwiper", {
     modifier: 1,
     slideShadows: false,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
 });
