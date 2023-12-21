1eb6088b047c886d7ed5ed38771212bfa89d9ac2
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-22 03:36:28 +0500

    map embedded

--- partials/reach.php
+++ partials/reach.php
@@ -1,1 +1,11 @@
-<h2 class="my-4 pt-4 text-center fw-bold h-font">Our Facilities</h2>
+<h2 class="my-4 pt-4 text-center fw-bold h-font">Reach Us</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+            <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14478.056090627351!2d67.01840118871739!3d24.880442462940085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e4505a98807%3A0xab98f715404df31b!2sGarden%20East%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1703197658668!5m2!1sen!2s" height="450" loading="lazy"></iframe>
+        </div>
+        <div class="col-md-4">
+
+        </div>
+    </div>
+</div>
