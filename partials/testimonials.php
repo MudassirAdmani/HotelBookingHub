e14e86d9c372bb715f53865b47b0ce73e82b4088
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-22 01:25:44 +0500

    testimonials completed

--- partials/testimonials.php
+++ partials/testimonials.php
@@ -1,76 +1,81 @@
 <h2 class="my-4 pt-4 text-center fw-bold h-font">Testimonials</h2>
 <div class="container">
-    <div class="swiper testimonialSwiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide bg-white p-4">
-                <div class="profile mb-3">
-                    <img src="./images/man-1.png" alt="">
-                    <h6 class="m-0 ms-2">John Doe</h6>
-                </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam cum sunt ullam incidunt tenetur voluptatum?
-                </p>
-                <div class="rating">
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                </div>
-            </div>
-            <div class="swiper-slide bg-white p-4">
-                <div class="profile mb-3">
-                    <img src="./images/man-2.png" alt="">
-                    <h6 class="m-0 ms-2">John Doe</h6>
-                </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam cum sunt ullam incidunt tenetur voluptatum?
-                </p>
-                <div class="rating">
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                </div>
-            </div>
-            <div class="swiper-slide bg-white p-4">
-                <div class="profile mb-3">
-                    <img src="./images/man-3.png" alt="">
-                    <h6 class="m-0 ms-2">John Doe</h6>
-                </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam cum sunt ullam incidunt tenetur voluptatum?
-                </p>
-                <div class="rating">
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                </div>
-            </div>
-            <div class="swiper-slide bg-white p-4">
-                <div class="profile mb-3">
-                    <img src="./images/woman-1.png" alt="">
-                    <h6 class="m-0 ms-2">John Doe</h6>
-                </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam cum sunt ullam incidunt tenetur voluptatum?
-                </p>
-                <div class="rating">
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
-                    <i class="bi bi-star-fill text-warning"></i>
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <div class="swiper testimonialSwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide d-flex align-items-center justify-content-center">
+                        <img src="./images/man-1.png" />
+                        <div>
+                            <h4 class="m-2">John Doe</h4>
+                            <p class="m-2">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                Laudantium obcaecati in aspernatur impedit quaerat perferendis?
+                            </p>
+                            <div class="rating m-2">
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide d-flex align-items-center justify-content-center">
+                        <img src="./images/man-2.png" />
+                        <div>
+                            <h4 class="m-2">Malik Zubair</h4>
+                            <p class="m-2">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                Laudantium obcaecati in aspernatur impedit quaerat perferendis?
+                            </p>
+                            <div class="rating m-2">
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide d-flex align-items-center justify-content-center">
+                        <img src="./images/man-3.png" />
+                        <div>
+                            <h4 class="m-2">Johnny Karlos</h4>
+                            <p class="m-2">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                Laudantium obcaecati in aspernatur impedit quaerat perferendis?
+                            </p>
+                            <div class="rating m-2">
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide d-flex align-items-center justify-content-center">
+                        <img src="./images/woman-1.png" />
+                        <div>
+                            <h4 class="m-2">Jane Smith</h4>
+                            <p class="m-2">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                Laudantium obcaecati in aspernatur impedit quaerat perferendis?
+                            </p>
+                            <div class="rating m-2">
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                                <i class="bi bi-star-fill text-warning fs-4"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="swiper-pagination"></div>
+        <div class="col-3"></div>
     </div>
 </div>
--- script.js
+++ script.js
@@ -8,30 +8,13 @@ var swiper = new Swiper(".mainSwiper", {
   },
 });
 var swiper2 = new Swiper(".testimonialSwiper", {
-  effect: "coverflow",
-  spaceBetween: 100,
+  effect: "cube",
   grabCursor: true,
-  centeredSlides: true,
   loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
+  cubeEffect: {
+    shadow: false,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
   },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  }
 });
