b997f7febba8ad24cbfaba8702b821e285ffd753
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-23 22:15:22 +0500

    reach page working

--- partials/reach.php
+++ partials/reach.php
@@ -4,8 +4,6 @@
         <div class="col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
             <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14478.056090627351!2d67.01840118871739!3d24.880442462940085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e4505a98807%3A0xab98f715404df31b!2sGarden%20East%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1703197658668!5m2!1sen!2s" height="450" loading="lazy"></iframe>
         </div>
-        <div class="col-md-4">
-
         </div>
     </div>
 </div>
