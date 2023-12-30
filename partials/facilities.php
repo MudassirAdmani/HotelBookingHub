8dda7b60f4d40039792a937bfb5cd2560ec75e08
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-31 02:28:10 +0500

    facilities animation added

--- partials/facilities.php
+++ partials/facilities.php
@@ -1,23 +1,23 @@
 <h2 class="my-4 pt-4 text-center fw-bold h-font">Our Facilities</h2>
 <div class="container">
     <div class="row justify-content-evenly px-md-0 px-5">
-        <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <div class="facilityCard col-md-2 text-center rounded shadow py-4">
             <img src="./images/wifi-1.png" alt="">
             <h5>Wifi</h5>
         </div>
-        <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <div class="facilityCard col-md-2 text-center rounded shadow py-4">
             <img src="./images/buffet-1.png" alt="">
             <h5>Buffet</h5>
         </div>
-        <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <div class="facilityCard col-md-2 text-center rounded shadow py-4">
             <img src="./images/kit-1.png" alt="">
             <h5>Hygiene Kit</h5>
         </div>
-        <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <div class="facilityCard col-md-2 text-center rounded shadow py-4">
             <img src="./images/sports-1.png" alt="">
             <h5>Sports</h5>
         </div>
-        <div class="col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <div class="facilityCard col-md-2 text-center rounded shadow py-4">
             <img src="./images/mic-1.png" alt="">
             <h5>Party</h5>
         </div>
