25af2f0722565eb60d32b620e2197aa208c09178
MudassirAdmani <mudassiradmani466@gmail.com>
2023-12-31 03:15:53 +0500

    contact us detail added

--- partials/reach.php
+++ partials/reach.php
@@ -2,9 +2,38 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-            <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14478.056090627351!2d67.01840118871739!3d24.880442462940085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e4505a98807%3A0xab98f715404df31b!2sGarden%20East%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1703197658668!5m2!1sen!2s" height="450" loading="lazy"></iframe>
+            <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14478.056090627351!2d67.01840118871739!3d24.880442462940085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e4505a98807%3A0xab98f715404df31b!2sGarden%20East%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1703197658668!5m2!1sen!2s" height="450" loading="lazy"></iframe>
         </div>
         <div class="col-md-4">
+            <div class="bg-white p-4 rounded mb-4">
+                <h5>Call Us</h5>
+                <a href="tel: +923120293322" class="d-inline-block mb-2 text-decoration-none text-dark">+923120293322</a>
+                <br>
+                <a href="tel: +923120293322" class="d-inline-block text-decoration-none text-dark">+923120293322</a>
+            </div>
+            <div class="bg-white p-4 rounded mb-4">
+                <h5>Follow Us</h5>
+                <a href="#" class="d-inline-block mb-3">
+                    <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-twitter me-1"></i>
+                        Twitter
+                    </span>
+                </a>
+                <br>
+                <a href="#" class="d-inline-block mb-3">
+                    <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-facebook me-1"></i>
+                        Facebook
+                    </span>
+                </a>
+                <br>
+                <a href="#" class="d-inline-block mb-3">
+                    <span class="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-instagram me-1"></i>
+                        Instagram
+                    </span>
+                </a>
+            </div>
         </div>
     </div>
 </div>
