d7ff37e97410d90bc0a473fb45632b2c965b71cc
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-03 02:13:57 +0500

    footer updated

--- partials/footer.php
+++ partials/footer.php
@@ -1,41 +1,65 @@
-<div class="container-fluid bg-white mt-5">
-    <div class="row">
-        <div class="col-lg-4 p-4">
-            <h3 class="h-font fs-3 fw-bold">HotelBookingHub</h3>
+<div class="container-fluid bg-dark mt-3">
+    <div class="row justify-content-center">
+        <div class="col-lg-4 p-4 text-white">
+            <h3 class="mb-3 h-font fs-3">HotelBookingHub</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, omnis dolorem perspiciatis, nesciunt
                 est dignissimos beatae repudiandae alias sint dolorum impedit quos doloribus nulla. Maiores iure natus
                 quas? Laborum, nostrum!</p>
         </div>
-        <div class="col-lg-4 p-4">
-            <h5 class="mb-3">Links</h5>
-            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a>
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 h-font fs-3">Links</h5>
+            <a href="#" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Home</a>
             <br>
-            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a>
+            <a href="#" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Rooms</a>
             <br>
-            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a>
+            <a href="#" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Facilities</a>
             <br>
-            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact</a>
+            <a href="#" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Contact</a>
             <br>
-            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            <a href="#" class="text-white d-inline-block mb-2 text-dark text-decoration-none">About</a>
         </div>
-        <div class="col-lg-4 p-4">
-            <h5 class="mb-3">Follow Us</h5>
-            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 fs-3 h-font">Follow Us</h5>
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
                 <i class="bi bi-instagram me-1"></i>
                 Instagram
             </a>
             <br>
-            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
                 <i class="bi bi-facebook me-1"></i>
                 Facebook
             </a>
             <br>
-            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
                 <i class="bi bi-twitter me-1"></i>
                 Twitter
             </a>
             <br>
         </div>
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 fs-3 h-font">Contact Us</h5>
+            <dl>
+                <dt>
+                    Phone:
+                </dt>
+                <dd>
+                    03120293322
+                </dd>
+                <dt>
+                    Email:
+                </dt>
+                <dd>
+                    mudassiradmani466@gmail.com
+                </dd>
+                <dt>
+                    Address:
+                </dt>
+                <dd>
+                    Garden East, Karachi Pakistan
+                </dd>
+            </dl>
+        </div>
+        <div class="col-lg-1"></div>
     </div>
 </div>
 <h6 class="text-center bg-dark text-white p-3 m-0">Copyrights @ Mudassir Admani</h6>
