6cab761a2bb7890870260bf015606ce55ae7cfaf
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-03 01:48:15 +0500

    footer completed

--- partials/footer.php
+++ partials/footer.php
@@ -1,13 +1,41 @@
 <div class="container-fluid bg-white mt-5">
     <div class="row">
         <div class="col-lg-4 p-4">
-
+            <h3 class="h-font fs-3 fw-bold">HotelBookingHub</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, omnis dolorem perspiciatis, nesciunt
+                est dignissimos beatae repudiandae alias sint dolorum impedit quos doloribus nulla. Maiores iure natus
+                quas? Laborum, nostrum!</p>
         </div>
         <div class="col-lg-4 p-4">
-            
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a>
+            <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a>
+            <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a>
+            <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact</a>
+            <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
         </div>
         <div class="col-lg-4 p-4">
-            
+            <h5 class="mb-3">Follow Us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-instagram me-1"></i>
+                Instagram
+            </a>
+            <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i>
+                Facebook
+            </a>
+            <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-twitter me-1"></i>
+                Twitter
+            </a>
+            <br>
         </div>
     </div>
 </div>
+<h6 class="text-center bg-dark text-white p-3 m-0">Copyrights @ Mudassir Admani</h6>
--- partials/testimonials.php
+++ partials/testimonials.php
@@ -10,7 +10,8 @@
                         <div>
                             <h4 class="m-2">John Doe</h4>
                             <p class="m-2 d-none d-lg-block">
-                                The hotel has a large bar, which made it convenient for our group to find each other after meetings. Highly recommend </p>
+                                The hotel has a large bar, which made it convenient for our group to find each other
+                                after meetings. Highly recommend </p>
                             <div class="rating m-2 d-flex flex-column flex-md-row">
                                 <i class="bi bi-star-fill text-warning fs-4"></i>
                                 <i class="bi bi-star-fill text-warning fs-4"></i>
@@ -25,7 +26,9 @@
                         <div>
                             <h4 class="m-2">Malik Zubair</h4>
                             <p class="m-2 d-none d-lg-block">
-                                I love this hotel! I never want to stay anywhere else! The rooms are spacious, clean, and comfortable. The beds are so comfortable that I slept like a baby. The view from my room was breathtaking
+                                I love this hotel! I never want to stay anywhere else! The rooms are spacious, clean,
+                                and comfortable. The beds are so comfortable that I slept like a baby. The view from my
+                                room was breathtaking
                             </p>
                             <div class="rating m-2 d-flex flex-column flex-md-row">
                                 <i class="bi bi-star-fill text-warning fs-4"></i>
@@ -41,7 +44,9 @@
                         <div>
                             <h4 class="m-2">Johnny Karlos</h4>
                             <p class="m-2 d-none d-lg-block">
-                                You were easily the best all-around hotel staff we have ever worked with. No one ever missed a beat, and no one frowned. Smiles from everyone and very gracious attitude. You could tell everyone there takes much pride in their work
+                                You were easily the best all-around hotel staff we have ever worked with. No one ever
+                                missed a beat, and no one frowned. Smiles from everyone and very gracious attitude. You
+                                could tell everyone there takes much pride in their work
                             </p>
                             <div class="rating m-2 d-flex flex-column flex-md-row">
                                 <i class="bi bi-star-fill text-warning fs-4"></i>
@@ -57,7 +62,10 @@
                         <div>
                             <h4 class="m-2">Jane Smith</h4>
                             <p class="m-2 d-none d-lg-block">
-                                The hotel staff was exceptional. We hosted several events at different hotels over the past years, but this one was the best. The F&B quality and presentation were outstanding, and the staff handled last-minute changes flawlessly. Attendees gave numerous compliments.
+                                The hotel staff was exceptional. We hosted several events at different hotels over the
+                                past years, but this one was the best. The F&B quality and presentation were
+                                outstanding, and the staff handled last-minute changes flawlessly. Attendees gave
+                                numerous compliments.
                             </p>
                             <div class="rating m-2 d-flex flex-column flex-md-row">
                                 <i class="bi bi-star-fill text-warning fs-4"></i>
@@ -72,5 +80,8 @@
             </div>
         </div>
         <div class="col-3"></div>
+        <div class="col-lg-12 text-center mt-4">
+            <a href="" class="py-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Know More</a>
+        </div>
     </div>
 </div>
