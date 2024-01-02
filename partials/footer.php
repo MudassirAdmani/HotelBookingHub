06525c88afa56458ce6439d2950f438c114f65cf
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-03 01:02:39 +0500

    footer started

---
+++ partials/footer.php
@@ -0,0 +1,13 @@
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+
+        </div>
+        <div class="col-lg-4 p-4">
+            
+        </div>
+        <div class="col-lg-4 p-4">
+            
+        </div>
+    </div>
+</div>
