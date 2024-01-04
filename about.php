9202c014766b634979de4e493bf6464229bdfa2f
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-05 02:04:23 +0500

    icons added

--- about.php
+++ about.php
@@ -31,30 +31,14 @@
     <?php
     include('./partials/carousel.php');
     ?>
-    <div class="my-5 px-4">
-        <h2 class="fw-bold h-font text-center">About Us</h2>
-        <div class="h-line btn-purple"></div>
-        <p class="text-center mt-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum <br>
-            sapiente quos unde consectetur blanditiis praesentium!
-        </p>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5">
-                <img src="./images/logo1.png" class="aboutImg" alt="">
-            </div>
-            <div class="col-lg-2"></div>
-            <div class="col-lg-5 mt-4 mt-lg-0 align-self-center">
-                <p class="fw-bold fs-5"><span class="text-purple">//</span>About Us</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, molestias eum fugiat illum quaerat sapiente! Enim laborum reiciendis sequi iure provident suscipit expedita illo? Enim optio expedita eius quis sit.</p>
-                <div class="d-flex justify-content-center justify-content-lg-start">
-                    <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Join Us</a>
-                    <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Room</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- About -->
+    <?php
+    include('./partials/about.php');
+    ?>
+    <!-- Features -->
+    <?php
+    include('./partials/features.php');
+    ?>
     <!-- Reach -->
     <?php
     include('./partials/reach.php');
---
+++ partials/features.php
@@ -0,0 +1,32 @@
+<div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">Features</h2>
+    <div class="h-line btn-purple"></div>
+</div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4 px-4">
+            <div class="bg-white rounded shadow p-4 border-orange text-center">
+                <img src="./images/hotel-1.png" alt="">
+                <h4 class="mt-3">100+ Rooms</h4>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 px-4">
+            <div class="bg-white rounded shadow p-4 border-orange text-center">
+                <img src="./images/person-1.png" alt="">
+                <h4 class="mt-3">200+ Visitors</h4>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 px-4">
+            <div class="bg-white rounded shadow p-4 border-orange text-center">
+                <img src="./images/hotel-1.png" alt="">
+                <h4 class="mt-3">150+ Reviews</h4>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 px-4">
+            <div class="bg-white rounded shadow p-4 border-orange text-center">
+                <img src="./images/hotel-1.png" alt="">
+                <h4 class="mt-3">100+ Rooms</h4>
+            </div>
+        </div>
+    </div>
+</div>
