8c61f85b76fd8aca58a02b7cb1f22e6e8a803dba
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-05 01:37:33 +0500

    about content

--- about.php
+++ about.php
@@ -18,6 +18,8 @@
     <link rel="stylesheet" href="./css/style.css">
     <!-- Navbar -->
     <link rel="stylesheet" href="./css/navbar.css">
+    <!-- About -->
+    <link rel="stylesheet" href="./css/about.css">
 </head>
 
 <body class="bg-light">
@@ -29,18 +31,34 @@
     <?php
     include('./partials/carousel.php');
     ?>
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">About Us</h2>
+        <div class="h-line btn-purple"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum <br>
+            sapiente quos unde consectetur blanditiis praesentium!
+        </p>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-lg-5">
-                <img src="./images/logo1.png" style="width: 600px; height: 400px; object-fit: cover;" alt="">
+                <img src="./images/logo1.png" class="aboutImg" alt="">
             </div>
             <div class="col-lg-2"></div>
-            <div class="col-lg-5 align-self-center">
+            <div class="col-lg-5 mt-4 mt-lg-0 align-self-center">
                 <p class="fw-bold fs-5"><span class="text-purple">//</span>About Us</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, molestias eum fugiat illum quaerat sapiente! Enim laborum reiciendis sequi iure provident suscipit expedita illo? Enim optio expedita eius quis sit.</p>
+                <div class="d-flex justify-content-center justify-content-lg-start">
+                    <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Join Us</a>
+                    <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Room</a>
+                </div>
             </div>
         </div>
     </div>
+    <!-- Reach -->
+    <?php
+    include('./partials/reach.php');
+    ?>
     <!-- Footer -->
     <?php
     include('./partials/footer.php');
