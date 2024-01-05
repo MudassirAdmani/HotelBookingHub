d4dab004258719f2d041bd75bd2c730f129bffb7
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-05 19:11:04 +0500

    icons complete

--- partials/features.php
+++ partials/features.php
@@ -18,14 +18,14 @@
         </div>
         <div class="col-lg-3 col-md-6 mb-4 px-4">
             <div class="bg-white rounded shadow p-4 border-orange text-center">
-                <img src="./images/hotel-1.png" alt="">
+                <img src="./images/review-1.png" alt="">
                 <h4 class="mt-3">150+ Reviews</h4>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 mb-4 px-4">
             <div class="bg-white rounded shadow p-4 border-orange text-center">
-                <img src="./images/hotel-1.png" alt="">
-                <h4 class="mt-3">100+ Rooms</h4>
+                <img src="./images/staff-1.png" alt="">
+                <h4 class="mt-3">50+ Staffs</h4>
             </div>
         </div>
     </div>
