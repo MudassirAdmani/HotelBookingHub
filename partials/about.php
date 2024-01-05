45d8740816fc47f010b5d14e6e1b6c32eaa99140
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-05 19:11:26 +0500

    About Paragraph added

--- partials/about.php
+++ partials/about.php
@@ -13,8 +13,16 @@
         </div>
         <div class="col-lg-2"></div>
         <div class="col-lg-5 mt-4 mt-lg-0 align-self-center">
-            <p class="fw-bold fs-5"><span class="text-purple">//</span>About Us</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, molestias eum fugiat illum quaerat sapiente! Enim laborum reiciendis sequi iure provident suscipit expedita illo? Enim optio expedita eius quis sit.</p>
+            <p class="fw-bold fs-5 text-center text-lg-start"><span class="text-purple">//</span>About Us</p>
+            <p class="text-center text-lg-start">Welcome to HotelBookingHub, your premier destination for seamless and
+                convenient hotel management. At HotelBookingHub, we are dedicated to
+                providing a user-friendly platform that simplifies the hotel booking
+                process for both travelers and hotel owners. With a focus on efficiency
+                and customer satisfaction, we strive to be the go-to solution for all
+                your hotel management needs. Whether you're a traveler seeking the perfect
+                accommodation or a hotel owner looking to maximize your reach, HotelBookingHub
+                is here to make the process effortless and enjoyable
+            </p>
             <div class="d-flex justify-content-center justify-content-lg-start">
                 <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Join Us</a>
                 <a href="" class="py-1 mx-1 text-decoration-none px-3 border-0 rounded-1 btn-orange text-white">Book Room</a>
