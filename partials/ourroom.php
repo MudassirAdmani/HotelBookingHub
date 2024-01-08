bd763cb352c1d092d2dfa51c9fe140dea8f1e97e
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-08 19:43:45 +0500

    first filter added

--- partials/ourroom.php
+++ partials/ourroom.php
@@ -5,7 +5,19 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-3">
-            
+            <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
+                <div class="container-fluid flex-lg-column align-items-stretch">
+                    <h4 class="mt-2">Filters</h4>
+                    <button class="navbar-toggler">
+                        <span class="navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="filterDropdown">
+                        <div class="border bg-light p-3 rounded mb-3">
+                            <h5 class="mb-3">Check Availability</h5>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     </div>
 </div>
