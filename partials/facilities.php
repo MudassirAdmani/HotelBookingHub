aa434af28c16322c839154613c733bc472c01c5d
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-08 19:56:49 +0500

    facility changes

--- partials/facilities.php
+++ partials/facilities.php
@@ -2,23 +2,23 @@
 <div class="h-line mb-4 btn-purple"></div>
 <div class="container">
     <div class="row justify-content-evenly px-md-0 px-5">
-        <div class="Card border-orange col-md-2 text-center rounded shadow py-4">
+        <div class="Card border-orange bg-white col-md-2 text-center rounded shadow py-4">
             <img src="./images/wifi-1.png" alt="">
             <h5>Wifi</h5>
         </div>
-        <div class="Card border-orange col-md-2 text-center rounded shadow py-4">
+        <div class="Card border-orange bg-white col-md-2 text-center rounded shadow py-4">
             <img src="./images/buffet-1.png" alt="">
             <h5>Buffet</h5>
         </div>
-        <div class="Card border-orange col-md-2 text-center rounded shadow py-4">
+        <div class="Card border-orange bg-white col-md-2 text-center rounded shadow py-4">
             <img src="./images/kit-1.png" alt="">
             <h5>Hygiene Kit</h5>
         </div>
-        <div class="Card border-orange col-md-2 text-center rounded shadow py-4">
+        <div class="Card border-orange bg-white col-md-2 text-center rounded shadow py-4">
             <img src="./images/sports-1.png" alt="">
             <h5>Sports</h5>
         </div>
-        <div class="Card border-orange col-md-2 text-center rounded shadow py-4">
+        <div class="Card border-orange bg-white col-md-2 text-center rounded shadow py-4">
             <img src="./images/mic-1.png" alt="">
             <h5>Party</h5>
         </div>
--- partials/ourroom.php
+++ partials/ourroom.php
@@ -7,13 +7,24 @@
         <div class="col-lg-3">
             <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
                 <div class="container-fluid flex-lg-column align-items-stretch">
-                    <h4 class="mt-2">Filters</h4>
+                    <h3 class="mt-2 text-center">Filters</h3>
                     <button class="navbar-toggler">
                         <span class="navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="filterDropdown">
-                        <div class="border bg-light p-3 rounded mb-3">
-                            <h5 class="mb-3">Check Availability</h5>
+                    <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="filterDropdown">
+                        <div class="p-3 rounded mb-3">
+                            <h5 class="mb-3">Availability</h5>
+                            <label class="form-label">Check In</label>
+                            <input type="date" class="form-control shadow-none">
+                            <label class="form-label">Check Out</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="p-3 rounded mb-3">
+                            <h5 class="mb-3">Facilities</h5>
+                            <div class="mb-2">
+                                <label class="form-label">Check In</label>
+                                <input type="date" class="form-control shadow-none">
+                            </div>
                         </div>
                     </div>
                 </div>
