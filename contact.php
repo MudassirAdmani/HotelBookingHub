fd34f28b2b69ab8319f014749ca89c769ff78fe5
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-12 03:20:45 +0500

    all links in dif folder

--- contact.php
+++ contact.php
@@ -5,19 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HotelBookingHub.com - Contact</title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <!-- Swiper -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <!-- CSS -->
-    <!-- Master -->
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- Navbar -->
-    <link rel="stylesheet" href="./css/navbar.css">
+    <?php
+    include('./include/links.php');
+    ?>
 </head>
 
 <body class="bg-light">
